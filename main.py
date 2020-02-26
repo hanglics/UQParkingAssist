0ba33bfd32821f962ebe67a9983387ce32895806
Hang Li <cecillll.lee@gmail.com>
2020-02-26 20:26:24 +1000

    add basic server info

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from uq_feed_helper import *
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jsonpify import jsonpify
+from middleware import *
+from waitress import serve
+import sys
+import json
+
+
+print("----------------------------------")
+print("Loading Parking Lot Info...")
+f = open("ParkingAddressMapper.json", "r")
+PARKINGLOTINFO = json.loads(f.read())
+f.close()
+print("Loaded!")
+print("----------------------------------")
 
 
 def main():
-    feedContent = getUQFeed()
-    parseFeed(feedContent)
+    feedContent = getUQFeed(PARKINGLOTINFO)
+    print(feedContent)
 
 
 if __name__ == '__main__':
--- uq_feed_helper.py
+++ uq_feed_helper.py
@@ -2,14 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getUQFeed():
+def getUQFeed(PARKINGLOTINFO):
     response = requests.get("https://pg.pf.uq.edu.au")
     if response.status_code == 200:
-        return response.content
+        return parseFeed(response.content, PARKINGLOTINFO)
     else:
-        getUQFeed()
+        getUQFeed(PARKINGLOTINFO)
 
 
-def parseFeed(content):
+def parseFeed(content, PARKINGLOTINFO):
+    data = []
+    status = []
     soup = BeautifulSoup(content, "html.parser")
-    print(soup.prettify())
+    table = soup.table
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+    for each in data:
+        if len(each) != 0:
+            status.append(each)
+    P10Status = None
+    for s in status:
+        if s[0] == "P10":
+            P10Status = s[1]
+    for parking in PARKINGLOTINFO:
+        for stat in status:
+            if parking["lot"] == stat[0]:
+                parking["status"] = stat[1]
+            elif parking["lot"] == "P10":
+                parking["status"] = P10Status
+    return PARKINGLOTINFO
